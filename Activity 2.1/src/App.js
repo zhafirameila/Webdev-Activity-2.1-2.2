b9555b8172cf6e9a127560368f9f542730875c29
Meila <162020370+zhafirameila@users.noreply.github.com>
2024-05-22 19:02:27 +0700

    Add files via upload
    Activity 2.1

---
+++ Activity 2.1/src/App.js
@@ -0,0 +1,24 @@
+
+import 'C:/Users/Meila/tugaswebdev/src/App.css';
+
+function App() {
+  return (
+  <div>
+    <header>
+      <h2> ⋆ Portfolio ⋆ </h2>
+    </header>
+    <header>
+      <h3>  Hi!! My Name is Meila ( ˶ˆᗜˆ˵ )  </h3>
+    </header>
+    <main>
+      <section>
+        <p>My full name is Zhafira Meila Hartanto and i am currently studying as a college student majoring Digital Business. </p>
+         <p>I am 18 years old and I'm a college student at Padjadjaran University.</p>
+         <p>I am interested and want to learn more about coding and designing ui/ux.  </p>
+      </section>
+    </main>
+  </div>
+  );
+  }
+
+export default App;
