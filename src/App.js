141c2a9954c9d449e090fa61e45f02ee668b4747
Meila <162020370+zhafirameila@users.noreply.github.com>
2024-05-22 23:15:54 +0700

    Add files via upload
    2.2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import GoalList from './components/goallist';
+import Header from './components/header';
+function App() {
+return (
+<>
+<Header />
+<GoalList />
+</>
+);
+}
+export default App;
---
+++ src/components/goallist.js
@@ -0,0 +1,16 @@
+
+import Firstgoal from './Firstgoal';
+import Secondgoal from './Secondgoal';
+import Thirdgoal from './Thirdgoal';
+
+function GoalList() {
+    return (
+        <div>
+           <Firstgoal />
+           <Secondgoal />
+           <Thirdgoal />
+        </div>
+    );
+}
+
+export default GoalList;
